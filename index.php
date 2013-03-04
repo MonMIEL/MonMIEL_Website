7b6690535f91d2358fcbcff04421941a7d12ec18
Vincent Burlet <vincent.burlet@voila.fr>
2013-03-04 18:00:40 +0100

    new file:   css/highchart/background.jpg 	new file:   css/highchart/main.css 	modified:   index.php 	new file:   js/highchart/dark-blue.js 	new file:   js/highchart/dark-green.js 	new file:   js/highchart/gray.js 	new file:   js/highchart/grid.js 	new file:   js/highchart/highcharts.js 	new file:   js/highchart/main.js 	new file:   js/highchart/skies.js

--- index.php
+++ index.php
@@ -1,54 +1,40 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width">
+<html lang="en" >
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>MonMIEL</title>
 
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <style>
-            body {
-                padding-top: 60px;
-                padding-bottom: 40px;
-            }
-        </style>
-        <link rel="stylesheet" href="css/bootstrap-responsive.min.css">
-        <link rel="stylesheet" href="css/main.css">
-		<link rel="stylesheet" href="css/style.css">
+    <!-- add styles -->
+    <link href="css/highchart/main.css" rel="stylesheet" type="text/css" />
 
-        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-    </head>
-    <body>
-		
-		<? include("header.php"); ?>
-		
-		<? include("menu.php"); ?>
+    <!-- add scripts -->
+    <script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+    <script src="js/highchart/highcharts.js"></script>
+    <script src="js/highchart/gray.js"></script>
+    <script src="js/highchart/main.js"></script>
+</head>
+<body>
 
-		<section>
-			<div id="Horizon">
-				<h1>Horizon</h1>
-				<label for="annee">Année sélectionnée</label>
-				<input id="annee" name="annee" type=text placeholder="exemple : 2012" required autofocus>
-			</div>
+<? include("header.php"); ?>
 
-			<div id="Scenario">
-				<h1>Scénario</h1>
-			</div>
+<? include("menu.php"); ?>
 
-			<div id="monmix">
-				<h1>Mon MIx ELectrique</h1>
-			</div>
 
-			<div id="simuler">
 
-			</div>
-		</section>
+<!-- Chart type switchers -->
+<div class="actions">
+    <button class="switcher" id="column">column</button>
+    <button class="switcher" id="area">area</button>
+    <button class="switcher" id="line">line</button>
+    <button class="switcher" id="spline">Spine</button>
+    <button class="switcher" id="areaspline">areaspline</button>
+</div>
 
-		<? include("footer.php"); ?>
-    </body>
+<!-- two different charts -->
+<div id="chart_3" class="chart"></div>
+<div id="chart_1" class="chart"></div>
+<div id="chart_2" class="chart"></div>
+</body>
+<? include("footer.php"); ?>
 </html>
