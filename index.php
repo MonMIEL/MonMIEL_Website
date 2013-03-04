809fa101eed41456cfd0617ba079161732ec4001
Abdelmalek KHETTOUF <a.khettouf@gmaill.com>
2013-03-04 17:36:19 +0100

    modified:   index.php

--- index.php
+++ index.php
@@ -8,16 +8,15 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title></title>
         <meta name="description" content="">
-        <meta name="viewport" content="width=device-width">
+        <!-- <meta name="viewport" content="width=device-width"> -->
 
-        <link rel="stylesheet" href="css/bootstrapcss">
+        <link rel="stylesheet" href="css/bootstrap.css">
         <style>
             body {
                 padding-top: 60px;
                 padding-bottom: 40px;
             }
         </style>
-        <!-- <link rel="stylesheet" href="css/bootstrap-responsive.min.css"> -->
         <link rel="stylesheet" href="css/main.css">
 		<link rel="stylesheet" href="css/style.css">
 
