89d2f661cb55907a8155f812bf4bb27745c7d59c
Florent Buonarota <florent.buonarota@gmail.com>
2013-03-05 10:40:03 +0100

    resoud conflit + encodage

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8;">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta charset="UTF-8" />
+		<meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
         <title></title>
         <meta name="description" content="">
         <link rel="stylesheet" href="css/bootstrap.css">
--- menu.php
+++ menu.php
@@ -1,12 +1,8 @@
 <div class="navbar" style="width:960px; margin:auto;">
   <div class="navbar-inner">
     <ul class="nav">
-<<<<<<< HEAD
 	  <li id="home"><a href="#"><img src="img/home.png" /></a></li>
-=======
-	  <li><a href="#" ><img src="img/home.png" style="width:20px;"/></a><li>
->>>>>>> c03f362b60e55a2a61fb4f31ab2f4f2ae3f2a9b4
-      <li><a href="#">Problematique</a></li>
+      <li><a href="#">Problématique</a></li>
       <li><a href="#">Simulez votre mix</a></li>
 	  <li><a href="#">Liens utiles</a></li>
 	  <li><a href="#">Contact</a></li>
