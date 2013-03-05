c03f362b60e55a2a61fb4f31ab2f4f2ae3f2a9b4
Abdelmalek KHETTOUF <a.khettouf@gmaill.com>
2013-03-05 09:55:01 +0100

    novueaux logos de partenaires avec transparence sur le footer

--- menu.php
+++ menu.php
@@ -1,7 +1,7 @@
 <div class="navbar" style="width:960px; margin:auto;">
   <div class="navbar-inner">
     <ul class="nav">
-	  <li><img src="img/home.png" style="width:30px;"/><li>
+	  <li><a href="#" ><img src="img/home.png" style="width:20px;"/></a><li>
       <li><a href="#">Problematique</a></li>
       <li><a href="#">Simulez votre mix</a></li>
 	  <li><a href="#">Liens utiles</a></li>
