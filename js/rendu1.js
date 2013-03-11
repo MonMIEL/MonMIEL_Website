efcbe3419b2cbccc3094a467401570b6151ca130
Vincent Burlet <vincent.burlet@voila.fr>
2013-03-11 15:19:33 +0100

    fusion des pages

--- js/rendu1.js
+++ js/rendu1.js
@@ -11,9 +11,91 @@ function chartselect( chart, index){
     chart.series[0].data[index].select(true);
 }
 
-var chart1, chart2;
 
-$(document).ready(function() {
+function passerEnergetique(){
+    document.getElementById('ariane').innerHTML="<img src='img/ariane3.png' />";
+    document.getElementById("rendue2").style.display="";
+    document.getElementById("parc").style.display="none";
+    document.getElementById("calcul").style.display="none";
+
+    miseEnPlaceHighChart();
+}
+
+var chart1, chart2,chart;
+
+function miseEnPlaceHighChart(){
+
+    chart = new Highcharts.Chart({
+        chart: {
+            renderTo: 'chart_conso',
+            type: 'area',
+            backgroundColor: 'rgba(255,255,255,0.5)'
+        },
+        title: {
+            text: 'Consommation'
+        },
+        subtitle: {
+            text: 'Année de reférence : 2050'
+        },
+        xAxis: {
+            categories: ['Janvier', 'Mars', 'Mai', 'Juillet', 'Septembre', 'Novembre', 'Decembre'],
+            tickmarkPlacement: 'on',
+            title: {
+                enabled: false
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Gw '
+            },
+            labels: {
+                formatter: function() {
+                    return this.value / 1000;
+                }
+            }
+        },
+        tooltip: {
+            formatter: function() {
+                return ''+
+                    this.x +': '+ Highcharts.numberFormat(this.y, 0, ',') +' GW';
+            }
+        },
+        plotOptions: {
+            area: {
+                stacking: 'normal',
+                lineColor: '#666666',
+                lineWidth: 1,
+                marker: {
+                    lineWidth: 1,
+                    lineColor: '#666666'
+                }
+            }
+        },
+        series: [{
+            name: 'Nucléaire',
+            data: [502, 635, 809, 947, 1402, 3634, 5268]
+        }, {
+            name: 'Photovoltaique',
+            data: [106, 107, 111, 133, 221, 767, 1766]
+        }, {
+            name: 'Eolien',
+            data: [163, 203, 276, 408, 547, 729, 628]
+        }, {
+            name: 'Hydraulique',
+            data: [18, 31, 54, 156, 339, 818, 1201]
+        }, {
+            name: 'Centrales à flammes',
+            data: [2, 2, 2, 6, 13, 30, 46]
+        }, {
+            name: 'STEP',
+            data: [0, 0, 0, 0, 0, 0, 0]
+        }, {
+            name: 'Import',
+            data: [0, 0, 0, 0, 0, 0, 0]
+        }]
+    });
+
+
     Highcharts.getOptions().colors = Highcharts.map(Highcharts.getOptions().colors, function(color) {
         return {
             radialGradient: { cx: 0.5, cy: 0.3, r: 0.7 },
@@ -149,6 +231,4 @@ $(document).ready(function() {
             ]
         }]
     });
-
-
-});
+};
