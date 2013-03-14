bceef8ea15b6750a7e51af9d9d09d36930f8e6ff
Florent Buonarota <florent.buonarota@gmail.com>
2013-03-14 15:18:19 +0100

    maj bulle

--- ihm1.php
+++ ihm1.php
@@ -132,7 +132,7 @@
 				Etape 1 - Scénario
 				
 			<!-- info bulle -->
-				<img src="img/bulle.png" class="bulle" tooltip="Bulle" style="height:25px; width:25px;" />
+				<img src="img/help.png" class="bulle" tooltip="Bulle" />
 			<!-- info bulle -->
 			
 			</h1>
