d04047ca4fa8b49a1fc03ba60f03f0ac317c4d94
Florent Buonarota <florent.buonarota@gmail.com>
2013-03-15 09:27:50 +0100

    ajout des bulles d'aides

--- rendu1.php
+++ rendu1.php
@@ -24,6 +24,10 @@
         <script src="js/calculJsonMonMIEL.js" ></script>
         <script src="js/oXHR.js"></script>
 		<script src="js/spin.js"></script>
+		<script type="text/javascript" src="js/jquery-1.3.2.min.js"></script>
+		<script type="text/javascript" src="js/jquery.qtip-1.0.0-rc3.min.js"></script>
+		<script type="text/javascript" src="js/infoBulle.js"></script>
+		
         <script>
             var nuc_gwh;
             var pho_gwh;
@@ -52,8 +56,6 @@
 			
         </script>
 		
-		<script type="text/javascript" src="js/infoBulle.js"></script>
-		
     </head>
     <body onload="init()">
 		<?php include("header.php"); ?>
@@ -67,9 +69,19 @@
 
 			<div id="loadCalculMonMIEL"></div>
 
-			<div id="parc">
+			<div id="parc" style="display:none;">
 
 				<h1>Résultat - Parc calculé</h1>
+				
+				<div class="aide">
+					<p>Ce tableau présente le parc calculé par le simulateur.</p>
+					<p>Le parc ciblé, celui qui a été saisi lors du paramétrage de la simulation, peut ne pas répondre au besoin en électricité. Il doit répondre à une règle importante :</p>
+					<p><Strong>Production = Consommation</Strong></p>
+					<p>La production représente l’ensemble de l’énergie générée par l’ensemble du parc électrique et la consommation ce qui est demandé par la population en électricité.</p>
+					<p>Si la production est supérieure à la consommation, le réseau ne supporte pas et risque de s’endommager voire de provoquer une coupure générale.</p>
+					<p>Si la production est inférieure à la demande, certains utilisateurs risquent de ne plus avoir d’électricité.</p>
+					</p>
+				</div>
 
 				<div class="bloc">
 					<div class="row first">
@@ -80,13 +92,13 @@
 						<div class="span2 evol">Evolution de la puissance (MW)</div>
 					</div>
 					<div class="row">
-						<div class="span2">Nucléaire <img src="img/help.png" class="bulle" tooltip="Bulle" /></div>
+						<div class="span2">Nucléaire <img src="img/help.png" class="bulle" tooltip="Le nucléaire fournit une puissance importante mais son fonctionnement ne permet pas de changer la production rapidement" /></div>
 						<div class="span6">
-							<div id="barParcTargetNuke" class="first progress progress-success progress-striped active">
+							<div id="barParcTargetNuke" class="first progress progress-success progress-striped active bulle" tooltip="Ce pourcentage représente la part du nucléaire sur le parc ciblé">
 							    <span class="value"></span>
                                 <div class="bar"></div>
 							</div>
-							<div id="barParcFinalNuke" class="progress progress-warning progress-striped active">
+							<div id="barParcFinalNuke" class="progress progress-warning progress-striped active bulle" tooltip="Ce pourcentage représenta la part du nucléaire sur le parc calculé par le simulateur">
                                 <span class="value"></span>
                                 <div class="bar"></div>
 							</div>
@@ -96,13 +108,13 @@
 						<div id="idDiffParcNuke" class="span1"></div>
 					</div>
 					<div class="row">
-						<div class="span2">Photovoltaïque</div>
+						<div class="span2">Photovoltaïque <img src="img/help.png" class="bulle" tooltip="Le photovoltaïque génère une électricité dites fatale. Autrement dit, on ne peut pas régler la puissance de l’électricité générée, il est donc nécessaire de la consommer" /></div>
 						<div class="span6">
-							<div id="barParcTargetPhoto" class="first progress progress-success progress-striped active">
+							<div id="barParcTargetPhoto" class="first progress progress-success progress-striped active bulle" tooltip="Ce pourcentage représente la part du photovoltaïque sur le parc ciblé">
                                 <span class="value"></span>
                                 <div class="bar"></div>
 							</div>
-							<div id="barParcFinalPhoto" class="progress progress-warning progress-striped active">
+							<div id="barParcFinalPhoto" class="progress progress-warning progress-striped active bulle" tooltip="Ce pourcentage représenta la part du photovoltaïque sur le parc calculé par le simulateur">
                                 <span class="value"></span>
                                 <div class="bar"></div>
 							</div>
@@ -112,13 +124,13 @@
                         <div id="idDiffParcPhoto" class="span1"></div>
 					</div>
 					<div class="row">
-						<div class="span2">Eolien</div>
+						<div class="span2">Eolien <img src="img/help.png" class="bulle" tooltip="Tout comme le photovoltaïque, l’éolien génère une électricité fatale" /></div>
 						<div class="span6">
-							<div id="barParcTargetEol" class="first progress progress-success progress-striped active">
+							<div id="barParcTargetEol" class="first progress progress-success progress-striped active bulle" tooltip="Ce pourcentage représente la part de l’éolien sur le parc ciblé">
                                 <span class="value"></span>
                                 <div class="bar"></div>
 							</div>
-							<div id="barParcFinalEol" class="progress progress-warning progress-striped active">
+							<div id="barParcFinalEol" class="progress progress-warning progress-striped active bulle" tooltip="Ce pourcentage représente la part de l’éolien sur le parc calculé par le simulateur">
                                 <span class="value"></span>
                                 <div class="bar"></div>
 							</div>
@@ -128,13 +140,13 @@
                         <div id="idDiffParcEol" class="span1"></div>
 					</div>
 					<div class="row">
-						<div class="span2">Hydraulique</div>
+						<div class="span2">Hydraulique <img src="img/help.png" class="bulle" tooltip="Tout comme le photovoltaïque, l’hydraulique génère une électricité fatale" /></div>
 						<div class="span6">
-							<div id="barParcTargetHydrau" class="first progress progress-success progress-striped active">
+							<div id="barParcTargetHydrau" class="first progress progress-success progress-striped active bulle" tooltip="Ce pourcentage représente la part de l’hydraulique sur le parc ciblé">
                                 <span class="value"></span>
                                 <div class="bar"></div>
 							</div>
-							<div id="barParcFinalHydrau" class="progress progress-warning progress-striped active">
+							<div id="barParcFinalHydrau" class="progress progress-warning progress-striped active bulle" tooltip="Ce pourcentage représenta la part de l’hydraulique sur le parc calculé par le simulateur">
                                 <span class="value"></span>
                                 <div class="bar"></div>
 							</div>
@@ -144,13 +156,13 @@
                         <div id="idDiffParcHydrau" class="span1"></div>
 					</div>
 					<div class="row">
-						<div class="span2">Centrales à flammes</div>
+						<div class="span2">Centrales à flammes <img src="img/help.png" class="bulle" tooltip="Les centrales à flammes ont la particularité de pouvoir régler la puissance générée rapidement. Autrement dit, lorsque la demande en électricité est forte durant un laps de temps court, les centrales à flammes permettent de combler le manque d’électricité" /></div>
 						<div class="span6">
-							<div id="barParcTargetFlame" class="first progress progress-success progress-striped active">
+							<div id="barParcTargetFlame" class="first progress progress-success progress-striped active bulle" tooltip="Ce pourcentage représente la part des centrales à flammes sur le parc ciblé">
                                 <span class="value"></span>
                                 <div class="bar"></div>
 							</div>
-							<div id="barParcFinalFlame" class="progress progress-warning progress-striped active">
+							<div id="barParcFinalFlame" class="progress progress-warning progress-striped active bulle" tooltip="Ce pourcentage représenta la part des centrales à flammes sur le parc calculé par le simulateur">
                                 <span class="value"></span>
                                 <div class="bar"></div>
 							</div>
@@ -160,13 +172,13 @@
                         <div id="idDiffParcFlame" class="span1"></div>
 					</div>
 					<div class="row">
-						<div class="span2">STEP</div>
+						<div class="span2">STEP <img src="img/help.png" class="bulle" tooltip="texte" /></div>
 						<div class="span6">
-							<div id="barParcTargetStep" class="first progress progress-success progress-striped active">
+							<div id="barParcTargetStep" class="first progress progress-success progress-striped active bulle" tooltip="Ce pourcentage représente la part des STEP sur le parc ciblé">
                                 <span class="value"></span>
                                 <div class="bar"></div>
 							</div>
-							<div id="barParcFinalStep" class="progress progress-warning progress-striped active">
+							<div id="barParcFinalStep" class="progress progress-warning progress-striped active bulle" tooltip="Ce pourcentage représenta la part des STEP sur le parc calculé par le simulateur">
                                 <span class="value"></span>
                                 <div class="bar"></div>
 							</div>
@@ -176,13 +188,13 @@
                         <div id="idDiffParcStep" class="span1"></div>
 					</div>
 					<div class="row">
-						<div class="span2">Import</div>
+						<div class="span2">Import <img src="img/help.png" class="bulle" tooltip="texte" /></div>
 						<div class="span6">
-							<div id="barParcTargetImport" class="first progress progress-success progress-striped active">
+							<div id="barParcTargetImport" class="first progress progress-success progress-striped active bulle" tooltip="Ce pourcentage représente la part de l’import sur le parc ciblé">
                                 <span class="value"></span>
                                 <div class="bar"></div>
 							</div>
-							<div id="barParcFinalImport" class="progress progress-warning progress-striped active">
+							<div id="barParcFinalImport" class="progress progress-warning progress-striped active bulle" tooltip="Ce pourcentage représenta la part de l’import sur le parc calculé par le simulateur">
                                 <span class="value"></span>
                                 <div class="bar"></div>
 							</div>
@@ -196,13 +208,13 @@
 						<div class="span1"></div>
 						<div class="span4 legende-verte">
                             Puissance énergétique du parc ciblé
-							<div id="idLegendeRef" class="progress progress-success progress-striped active">
+							<div id="idLegendeRef" class="progress progress-success progress-striped active bulle" tooltip="Le parc ciblé est celui que vous avez choisi lors du paramétrage de la simulation">
 							  <div class="bar" style="width: 100%;"></div>
 							</div>
 						</div>
 						<div class="span4 legende-jaune">
                             Puissance énergétique du parc calculé
-							<div id="idLegendeCible" class="progress progress-warning progress-striped active">
+							<div id="idLegendeCible" class="progress progress-warning progress-striped active bulle" tooltip="Le parc calculé est le parc ciblé avec les ajustements fait par le simulateur. Il peut y avoir un écart important si le parc ciblé n’était pas assez flexible dans sa capacité à répondre à la demande">
 							  <div class="bar" style="width: 100%;"></div>
 							</div>
 						</div>
